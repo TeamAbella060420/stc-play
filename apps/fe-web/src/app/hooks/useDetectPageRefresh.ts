a7f1caae45f116643d0a1806d36a0482f2f654a6
TeamAbella060420 <teamabella@outlook.com>
2023-08-17 08:00:12 +0800

    Update useDetectPageRefresh.ts

--- apps/fe-web/src/app/hooks/useDetectPageRefresh.ts
+++ apps/fe-web/src/app/hooks/useDetectPageRefresh.ts
@@ -1,28 +1,22 @@
 import { useEffect } from "react";
 
-const usePageRefreshDetection = () =>
-{
-  const hasPageBeenRefreshed = () =>
-  {
+const usePageRefreshDetection = () => {
+  const hasPageBeenRefreshed = () => {
     return !!localStorage.getItem("isPageRefresh")
   }
 
-  const reset = () =>
-  {
+  const reset = () => {
     localStorage.removeItem("isPageRefresh")
   }
 
-  useEffect(() =>
-  {
-    const storeRefreshState = () =>
-    {
+  useEffect(() => {
+    const storeRefreshState = () => {
       localStorage.setItem("isPageRefresh", (true).toString())
     }
 
     window.addEventListener("beforeunload", storeRefreshState);
 
-    return () =>
-    {
+    return () => {
       window.removeEventListener("beforeunload", storeRefreshState);
     };
   }, []);
