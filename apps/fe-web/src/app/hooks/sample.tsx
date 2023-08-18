81f9700361be5a39630a0a3fcd4e18565acbdbdb
TeamAbella060420 <teamabella@outlook.com>
2023-08-18 16:02:15 +0800

    Create sample.tsx

---
+++ apps/fe-web/src/app/hooks/sample.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import useGTM from './useGTM';
+
+// function SomeComponent() {
+//   const gtm = useGTM();
+
+//   const trackButtonClick = () => {
+//     gtm.addEvent({
+//       event: 'buttonClick',
+//       event_properties: { action: 'click', label: 'Button A' },
+//       user_properties: { userId: '12345' }
+//     });
+//   };
+
+// }
